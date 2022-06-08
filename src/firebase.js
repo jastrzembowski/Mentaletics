8c6f7c915ad430cf9265c12054ffcc9304b84c63
Kamil <jastrzembowskikamil@gmail.com>
2022-06-08 11:44:50 +0200

    small changes

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-t9t071AJTC5VKcp7lOIOZQIrif51NFQ",
+  authDomain: "mentaletics-485cb.firebaseapp.com",
+  projectId: "mentaletics-485cb",
+  storageBucket: "mentaletics-485cb.appspot.com",
+  messagingSenderId: "764277144748",
+  appId: "1:764277144748:web:e276b573b2fc1f51287309"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
